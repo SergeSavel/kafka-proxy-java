f03a2955c7c391af3092845c23c3a80e0988984e
Serge Savel <serge@savel.pro>
2026-04-02 16:19:55 +0400

    Deps upgrade

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,19 +21,19 @@ repositories {
 }
 
 dependencies {
-    implementation("io.netty:netty-all:4.2.7.Final")
-    implementation("org.apache.kafka:kafka-clients:4.1.1")
+    implementation("io.netty:netty-all:4.2.12.Final")
+    implementation("org.apache.kafka:kafka-clients:4.1.2")
     implementation("org.slf4j:slf4j-api:2.0.17")
-    implementation(platform("org.apache.logging.log4j:log4j-bom:2.25.2"))
+    implementation(platform("org.apache.logging.log4j:log4j-bom:2.25.4"))
     runtimeOnly("org.apache.logging.log4j:log4j-core")
     runtimeOnly("org.apache.logging.log4j:log4j-layout-template-json")
     runtimeOnly("org.apache.logging.log4j:log4j-slf4j2-impl")
-    compileOnly("org.projectlombok:lombok:1.18.42")
-    annotationProcessor("org.projectlombok:lombok:1.18.42")
-    testCompileOnly("org.projectlombok:lombok:1.18.42")
-    testAnnotationProcessor("org.projectlombok:lombok:1.18.42")
-    implementation("com.fasterxml.jackson.core:jackson-databind:2.20.1")
-    testImplementation(platform("org.junit:junit-bom:5.14.1"))
+    compileOnly("org.projectlombok:lombok:1.18.44")
+    annotationProcessor("org.projectlombok:lombok:1.18.44")
+    testCompileOnly("org.projectlombok:lombok:1.18.44")
+    testAnnotationProcessor("org.projectlombok:lombok:1.18.44")
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.21.2")
+    testImplementation(platform("org.junit:junit-bom:5.14.3"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
 
